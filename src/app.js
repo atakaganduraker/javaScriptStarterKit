ad4c8e05c4d0389860d5106bd780da5c45e75d41
atakagnn <you@example.com>
2021-10-24 22:49:02 +0300

    jscamp

---
+++ src/app.js
@@ -0,0 +1,39 @@
+console.log("merhaba kodlama.io")
+
+let dolarBugun = 9.30  
+let dolarDun = 9.20
+{
+    let dolarDun=9.10
+
+}
+
+console.log(dolarDun)
+
+
+const euroDun=11.2 
+//euroDun=11 bunu yapamiyoruz const yuzunden
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasin
+
+let konutKredileri=["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Ozel konut kredisi"] 
+
+console.log("<ul>")
+
+for (let i = 0; i <konutKredileri.length ; i++) 
+{
+  console.log("<li>"+konutKredileri[i]+"</li>")  
+}
+console.log("</u>")
+
+// <ul>
+//     <li>Konut kredisi</li>
+//     <li>Emlak Konut kredisi</li>
+//     <li>Kamu Konut kredisi</li>
+// </ul>
+
+console.log(konutKredileri)
+
